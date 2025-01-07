be82f07492f2f1d9decba7bc266f380040ce182d
Jeremy Ellis <jeremyellis173@gmail.com>
2025-01-07 15:20:38 -0500

    removing footer

--- src/components/CalendarFooter/CalendarFooter.tsx
+++ src/components/CalendarFooter/CalendarFooter.tsx
@@ -3,7 +3,27 @@ import styles from './CalendarFooter.module.css'
 export default function CalendarFooter(){
   return(
       <div className={styles.footerDiv}>
-      .
+        <div className={styles.dayDivSun}>
+          .
+        </div>
+        <div className={styles.dayDiv}>
+          
+        </div>
+        <div className={styles.dayDiv}>
+          
+        </div>
+        <div className={styles.dayDiv}>
+          
+        </div>
+        <div className={styles.dayDiv}>
+          
+        </div>
+        <div className={styles.dayDiv}>
+          
+        </div>
+        <div className={styles.dayDivSat}>
+          
+        </div>
       </div>
   )
 }
