ab6a0492131bf43a242a4df6fc4ec31b3f345a4e
Jeremy Ellis <jeremyellis173@gmail.com>
2025-01-06 20:04:59 -0500

    added day headers

--- src/components/CalendarHeaderDays/CalendarHeaderDays.tsx
+++ src/components/CalendarHeaderDays/CalendarHeaderDays.tsx
@@ -2,8 +2,28 @@ import styles from './CalendarHeaderDays.module.css'
 
 export default function CalendarHeader(){
   return(
-      <div className={styles.days}>
-        days
+      <div className={styles.daysDiv}>
+        <div className={styles.dayDiv}>
+          SUN
+        </div>
+        <div className={styles.dayDiv}>
+          MON
+        </div>
+        <div className={styles.dayDiv}>
+          TUE
+        </div>
+        <div className={styles.dayDiv}>
+          WED
+        </div>
+        <div className={styles.dayDiv}>
+          THU
+        </div>
+        <div className={styles.dayDiv}>
+          FRI
+        </div>
+        <div className={styles.dayDiv}>
+          SAT
+        </div>
       </div>
   )
 }
