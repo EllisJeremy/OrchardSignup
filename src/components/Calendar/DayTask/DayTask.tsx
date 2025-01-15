969435fff015428d1499b61a90ef4f72174a3a1b
Jeremy Ellis <jeremyellis173@gmail.com>
2025-01-14 22:09:47 -0500

    fixed task overflow

--- src/components/Calendar/DayTask/DayTask.tsx
+++ src/components/Calendar/DayTask/DayTask.tsx
@@ -12,18 +12,37 @@ export default function DayTask(props: propsType) {
 
   if (taskDatabase.has(props.cellDate)){
     const tasks = taskDatabase.get(props.cellDate)!;
-
-      return (
-        
-        <>
-          {tasks.map((tasks, index) => (  
-          <div key={index} className={styles.dayContentChildDiv}>
-            <p className={styles.taskLabel1}>{tasks.title}</p>
-          </div>
+      if (tasks.length < 5){
+        return (
           
-          ))}
-        </>
-      )
+          <>
+            {tasks.map((tasks, index) => (  
+            <div key={index} className={styles.dayContentChildDiv}>
+              <p className={styles.taskLabel}>{tasks.title}</p>
+            </div>
+            
+            ))}
+          </>
+        )
+      }
+
+      if (tasks.length >= 5){
+        return (
+          
+          <>
+            {tasks.slice(0,3).map((tasks, index) => (  
+            <div key={index} className={styles.dayContentChildDiv}>
+              <p className={styles.taskLabel}>{tasks.title}</p>
+            </div>
+            
+            ))}
+
+            <div  className={styles.dayContentChildDiv}>
+              <p className={styles.overflowLabel}>{tasks.length - 3} more</p>
+            </div>
+          </>
+        )
+      }
   }
 
   
