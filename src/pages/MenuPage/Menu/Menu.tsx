ff72481328305aadaa3344ebd150e670bf258839
Jeremy Ellis <jeremyellis173@gmail.com>
2025-06-26 23:13:07 -0400

    feat: menu page 1/2 done

--- src/pages/MenuPage/Menu/Menu.tsx
+++ src/pages/MenuPage/Menu/Menu.tsx
@@ -1,23 +1,50 @@
 import styles from "./Menu.module.css";
 import { Link } from "react-router-dom";
+import map from "../../../assets/map.svg";
+import writeCheck from "../../../assets/writeCheck.svg";
+import bank from "../../../assets/bank.svg";
 
 export default function Menu() {
   return (
     <div className={styles.mainDiv}>
-      <div className={styles.loginDiv}>
-        <div className={styles.headerDiv}>
-          <img className={styles.orchardLogo} />
+      <div className={styles.sectionDiv}>
+        <div className={styles.gridDiv}>
+          <div className={styles.tile}>
+            <>
+              <h2 className={styles.title}>Reference Material</h2>
+              <img className={styles.logo} src={map} />
+            </>
+          </div>
+          <Link className={styles.tile} to="/Menu/Calendar">
+            Calendar
+          </Link>
         </div>
-
-        <button className={styles.loginButton} type="submit">
-          Sign up
-        </button>
+      </div>
+      <div className={styles.sectionDiv}>
+        <div className={styles.headerDiv}>
+          <h2 className={styles.title}>Treasurer's report</h2>
+          <img className={styles.logo} src={bank} />
+        </div>
+        <div className={styles.gridDiv}>
+          <div className={styles.tile}></div>
+          <Link className={styles.tile} to="/Menu/Calendar">
+            Calendar
+          </Link>
+        </div>
       </div>
 
-      <div className={styles.signupDiv}>
-        <Link className={styles.loginButton} to="/Menu/Calendar">
-          Calendar
-        </Link>
+      <div className={styles.sectionDiv}>
+        <div className={styles.headerDiv}>
+          <h2 className={styles.title}>Financial Management</h2>
+          <img className={styles.logo} src={writeCheck} />
+        </div>
+
+        <div className={styles.gridDiv}>
+          <div className={styles.tile}></div>
+          <Link className={styles.tile} to="/Menu/Calendar">
+            Calendar
+          </Link>
+        </div>
       </div>
     </div>
   );
