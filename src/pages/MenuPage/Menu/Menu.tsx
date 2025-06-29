aeb51581ee1519bbfdddb12dcfe6663e8e1afa3e
Jeremy Ellis <jeremyellis173@gmail.com>
2025-06-29 18:12:49 -0400

    feat: more menu

--- src/pages/MenuPage/Menu/Menu.tsx
+++ src/pages/MenuPage/Menu/Menu.tsx
@@ -6,44 +6,46 @@ import bank from "../../../assets/bank.svg";
 
 export default function Menu() {
   return (
-    <div className={styles.mainDiv}>
-      <div className={styles.sectionDiv}>
-        <div className={styles.gridDiv}>
-          <div className={styles.tile}>
-            <>
-              <h2 className={styles.title}>Reference Material</h2>
-              <img className={styles.logo} src={map} />
-            </>
+    <div className={styles.bodyDiv}>
+      <div className={styles.mainDiv}>
+        <div className={styles.sectionDiv}>
+          <div className={styles.gridDiv}>
+            <div className={styles.tile}>
+              <>
+                <h2 className={styles.title}>Reference Material</h2>
+                <img className={styles.logo} src={map} />
+              </>
+            </div>
+            <Link className={styles.tile} to="/Menu/Calendar">
+              Calendar
+            </Link>
           </div>
-          <Link className={styles.tile} to="/Menu/Calendar">
-            Calendar
-          </Link>
         </div>
-      </div>
-      <div className={styles.sectionDiv}>
-        <div className={styles.headerDiv}>
-          <h2 className={styles.title}>Treasurer's report</h2>
-          <img className={styles.logo} src={bank} />
-        </div>
-        <div className={styles.gridDiv}>
-          <div className={styles.tile}></div>
-          <Link className={styles.tile} to="/Menu/Calendar">
-            Calendar
-          </Link>
-        </div>
-      </div>
-
-      <div className={styles.sectionDiv}>
-        <div className={styles.headerDiv}>
-          <h2 className={styles.title}>Financial Management</h2>
-          <img className={styles.logo} src={writeCheck} />
+        <div className={styles.sectionDiv}>
+          <div className={styles.headerDiv}>
+            <h2 className={styles.title}>Treasurer's report</h2>
+            <img className={styles.logo} src={bank} />
+          </div>
+          <div className={styles.gridDiv}>
+            <div className={styles.tile}></div>
+            <Link className={styles.tile} to="/Menu/Calendar">
+              Calendar
+            </Link>
+          </div>
         </div>
 
-        <div className={styles.gridDiv}>
-          <div className={styles.tile}></div>
-          <Link className={styles.tile} to="/Menu/Calendar">
-            Calendar
-          </Link>
+        <div className={styles.sectionDiv}>
+          <div className={styles.headerDiv}>
+            <h2 className={styles.title}>Financial Management</h2>
+            <img className={styles.logo} src={writeCheck} />
+          </div>
+
+          <div className={styles.gridDiv}>
+            <div className={styles.tile}></div>
+            <Link className={styles.tile} to="/Menu/Calendar">
+              Calendar
+            </Link>
+          </div>
         </div>
       </div>
     </div>
