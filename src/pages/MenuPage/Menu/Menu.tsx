9f28a7d3ce609e481f8d46208b4626d16c031b64
Jeremy Ellis <jeremyellis173@gmail.com>
2025-06-30 09:11:00 -0400

    feat: for ui for menu

--- src/pages/MenuPage/Menu/Menu.tsx
+++ src/pages/MenuPage/Menu/Menu.tsx
@@ -1,5 +1,5 @@
 import styles from "./Menu.module.css";
-import calendarImg from 
+import calendarScreenshot from "../../../assets/calendarScreenshot.png";
 import { Link } from "react-router-dom";
 
 export default function Menu() {
@@ -7,19 +7,74 @@ export default function Menu() {
     <div className={styles.bodyDiv}>
       <div className={styles.mainDiv}>
         <div className={styles.calendarDiv}>
-          <div className={styles.calendarHeaderDiv}>
+          <div className={styles.headerDiv}>
             <h2>Calendar</h2>
             <h3>View events and sign up for tasks</h3>
           </div>
           <div className={styles.calendarPictureDiv}>
-            <img src={calendarImg}></img>
+            <img className={styles.calendarPicture} src={calendarScreenshot}></img>
           </div>
         </div>
 
-        <div className={styles.financeDiv}></div>
-        <div className={styles.requestDiv}></div>
-        <div className={styles.bylawsDiv}></div>
-        <div className={styles.adminDiv}></div>
+        <div className={styles.financeDiv}>
+          <div className={styles.headerDiv} style={{ justifyContent: "center" }}>
+            <h2>Finances</h2>
+            <h3></h3>
+          </div>
+          <div className={styles.financeContainerDiv}>
+            <div className={styles.financeItemDiv}>
+              <div className={styles.financeItemTitleDiv}>Financial Summary</div>
+            </div>
+            <div className={styles.financeItemDiv}>
+              <div className={styles.financeItemTitleDiv}>Weekly Contributions Summary</div>
+            </div>
+            <div className={styles.financeItemDiv}>
+              <div className={styles.financeItemTitleDiv}>Budget Status</div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.requestDiv}>
+          <div className={styles.headerDiv}>
+            <h2 style={{ fontSize: "20px" }}>Requests</h2>
+            <h3></h3>
+          </div>
+        </div>
+        <div className={styles.bylawsDiv}>
+          <div className={styles.headerDiv}>
+            <h2 style={{ fontSize: "20px" }}>Bylaws</h2>
+            <h3></h3>
+          </div>
+        </div>
+        <div className={styles.adminDiv}>
+          <div className={styles.headerDiv} style={{ justifyContent: "center" }}>
+            <h2>Admin</h2>
+            <h3></h3>
+          </div>
+          <div className={styles.adminContainerDiv}>
+            <div className={styles.adminSplitDiv}>
+              <div className={styles.adminItemLeftDiv}>
+                <div className={styles.adminItemTitleDiv}>Financial Summary</div>
+              </div>
+              <div className={styles.adminItemLeftDiv}>
+                <div className={styles.adminItemTitleDiv}>Weekly Contributions Summary</div>
+              </div>
+              <div className={styles.adminItemLeftDiv}>
+                <div className={styles.adminItemTitleDiv}>Budget Status</div>
+              </div>
+            </div>
+            <div className={styles.adminSplitDiv}>
+              <div className={styles.adminItemRightDiv}>
+                <div className={styles.adminItemTitleDiv}>Financial Summary</div>
+              </div>
+              <div className={styles.adminItemRightDiv}>
+                <div className={styles.adminItemTitleDiv}>Weekly Contributions Summary</div>
+              </div>
+              <div className={styles.adminItemRightDiv}>
+                <div className={styles.adminItemTitleDiv}>Budget Status</div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
