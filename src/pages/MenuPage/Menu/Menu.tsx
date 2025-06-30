73b76679e90244b084029329f52f97626d6fe518
Jeremy Ellis <jeremyellis173@gmail.com>
2025-06-29 21:05:45 -0400

    feat: menu now uses bento box

--- src/pages/MenuPage/Menu/Menu.tsx
+++ src/pages/MenuPage/Menu/Menu.tsx
@@ -1,52 +1,25 @@
 import styles from "./Menu.module.css";
+import calendarImg from 
 import { Link } from "react-router-dom";
-import map from "../../../assets/map.svg";
-import writeCheck from "../../../assets/writeCheck.svg";
-import bank from "../../../assets/bank.svg";
 
 export default function Menu() {
   return (
     <div className={styles.bodyDiv}>
       <div className={styles.mainDiv}>
-        <div className={styles.sectionDiv}>
-          <div className={styles.gridDiv}>
-            <div className={styles.tile}>
-              <>
-                <h2 className={styles.title}>Reference Material</h2>
-                <img className={styles.logo} src={map} />
-              </>
-            </div>
-            <Link className={styles.tile} to="/Menu/Calendar">
-              Calendar
-            </Link>
+        <div className={styles.calendarDiv}>
+          <div className={styles.calendarHeaderDiv}>
+            <h2>Calendar</h2>
+            <h3>View events and sign up for tasks</h3>
           </div>
-        </div>
-        <div className={styles.sectionDiv}>
-          <div className={styles.headerDiv}>
-            <h2 className={styles.title}>Treasurer's report</h2>
-            <img className={styles.logo} src={bank} />
-          </div>
-          <div className={styles.gridDiv}>
-            <div className={styles.tile}></div>
-            <Link className={styles.tile} to="/Menu/Calendar">
-              Calendar
-            </Link>
+          <div className={styles.calendarPictureDiv}>
+            <img src={calendarImg}></img>
           </div>
         </div>
 
-        <div className={styles.sectionDiv}>
-          <div className={styles.headerDiv}>
-            <h2 className={styles.title}>Financial Management</h2>
-            <img className={styles.logo} src={writeCheck} />
-          </div>
-
-          <div className={styles.gridDiv}>
-            <div className={styles.tile}></div>
-            <Link className={styles.tile} to="/Menu/Calendar">
-              Calendar
-            </Link>
-          </div>
-        </div>
+        <div className={styles.financeDiv}></div>
+        <div className={styles.requestDiv}></div>
+        <div className={styles.bylawsDiv}></div>
+        <div className={styles.adminDiv}></div>
       </div>
     </div>
   );
