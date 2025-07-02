3f50dc30aa294ade4c98677c49a5f26ce9ad8015
Jeremy Ellis <jeremyellis173@gmail.com>
2025-07-02 00:15:27 -0400

    feat: add links

--- src/pages/MenuPage/Menu/Menu.tsx
+++ src/pages/MenuPage/Menu/Menu.tsx
@@ -20,120 +20,130 @@ import calendar from "../../../assets/calendar.svg";
 import recentMessage from "../../../assets/recentMessage.jpg";
 
 import { Link } from "react-router-dom";
+
 const admin: boolean = false;
+
 export default function Menu() {
   return (
     <div className={styles.bodyDiv}>
       <div className={styles.mainDiv}>
         {/* Calendar */}
-        <div className={styles.calendarDiv}>
+        <Link to="/menu/calendar" className={styles.calendarDiv}>
           <div className={styles.headerDiv}>
             <h2>Calendar</h2>
-            <img src={calendar}></img>
+            <img src={calendar} />
           </div>
           <div className={styles.calendarPictureDiv}>
             <img className={styles.calendarPicture} src={calendarScreenshot} alt="Calendar" />
           </div>
-        </div>
-        {/* log in */}
+        </Link>
+
+        {/* Log in / Sign up */}
         <div className={styles.accountContainerDiv}>
-          <div className={styles.accountDiv}>
+          <Link to="/" className={styles.accountDiv}>
             <div className={styles.headerDiv}>
-              <img src={logIn}></img>
+              <img src={logIn} />
               <h2>Log in</h2>
             </div>
-          </div>
-          <div className={styles.accountDiv}>
+          </Link>
+          <Link to="/signup" className={styles.accountDiv}>
             <div className={styles.headerDiv}>
-              <img src={signUp}></img>
+              <img src={signUp} />
               <h2>Sign up</h2>
             </div>
-          </div>
+          </Link>
         </div>
-        {/* Purple / Messages */}
-        <div className={styles.messagesDiv}>
+
+        {/* Messages */}
+        <Link to="/menu/calendar" className={styles.messagesDiv}>
           <div className={styles.headerDiv}>
-            <img src={all}></img>
+            <img src={all} />
             <h2>All Messages</h2>
           </div>
-          <h3 style={{ textAlign: "right" }}> View past weeks sermons</h3>
-        </div>
-        <div className={styles.messagesDiv}>
+          <h3 style={{ textAlign: "right" }}>View past week's sermons</h3>
+        </Link>
+
+        <Link to="/menu/calendar" className={styles.messagesDiv}>
           <div className={styles.headerDiv}>
-            <img src={clock}></img>
+            <img src={clock} />
             <h2>Recent Message</h2>
           </div>
           <div className={styles.messagePictureDiv}>
             <img className={styles.calendarPicture} src={recentMessage} alt="Calendar" />
           </div>
-        </div>
+        </Link>
+
         {/* Finance */}
         <div className={styles.financeDiv}>
           <div className={styles.headerDiv}>
             <h2>Finances</h2>
           </div>
           <div className={styles.ContainerDiv}>
-            <div className={styles.financeItemDiv}>
+            <Link to="/menu/calendar" className={styles.financeItemDiv}>
               <h2 style={{ fontSize: "20px" }}>Financial Summary</h2>
-              <img src={dollar} style={{ height: "50%" }}></img>
-            </div>
-            <div className={styles.financeItemDiv}>
+              <img src={dollar} style={{ height: "50%" }} />
+            </Link>
+            <Link to="/menu/calendar" className={styles.financeItemDiv}>
               <h2 style={{ fontSize: "20px" }}>Weekly Contributions</h2>
-              <img src={week} style={{ height: "50%" }}></img>
-            </div>
-            <div className={styles.financeItemDiv}>
+              <img src={week} style={{ height: "50%" }} />
+            </Link>
+            <Link to="/menu/calendar" className={styles.financeItemDiv}>
               <h2 style={{ fontSize: "20px" }}>Budget Status</h2>
-              <img src={paper} style={{ height: "50%" }}></img>
-            </div>
+              <img src={paper} style={{ height: "50%" }} />
+            </Link>
           </div>
         </div>
+
         {/* Requests */}
-        <div className={styles.requestDiv}>
+        <Link to="/menu/calendar" className={styles.requestDiv}>
           <div className={styles.headerDiv}>
             <h2>Requests Disbursements</h2>
-            <img src={refund}></img>
+            <img src={refund} />
           </div>
           <h3>Get a refund for your donation</h3>
-        </div>
+        </Link>
+
         {/* Bylaws */}
-        <div className={styles.bylawsDiv}>
+        <Link to="/menu/calendar" className={styles.bylawsDiv}>
           <div className={styles.headerDiv}>
             <h2>Bylaws</h2>
-            <img src={scroll}></img>
+            <img src={scroll} />
           </div>
           <h3>View the rules of the church</h3>
-        </div>
+        </Link>
+
         {/* Admin */}
         {admin && (
           <div className={styles.adminAuthDiv}>
             <h2>You do not have admin access</h2>
           </div>
         )}
+
         <div className={styles.adminDiv}>
           <div className={styles.headerDiv} style={{ justifyContent: "right" }}>
             <h2>Admin</h2>
           </div>
           <div className={styles.ContainerDiv}>
-            <div className={styles.adminItemDiv}>
-              <img src={refund} style={{ height: "60%" }}></img>
+            <Link to="/menu/calendar" className={styles.adminItemDiv}>
+              <img src={refund} style={{ height: "60%" }} />
               View Contributions
-            </div>
-            <div className={styles.adminItemDiv}>
-              <img src={check} style={{ height: "60%" }}></img>
+            </Link>
+            <Link to="/menu/calendar" className={styles.adminItemDiv}>
+              <img src={check} style={{ height: "60%" }} />
               Approve Disbursements
-            </div>
-            <div className={styles.adminItemDiv}>
-              <img src={handMoney} style={{ height: "60%" }}></img>
+            </Link>
+            <Link to="/menu/calendar" className={styles.adminItemDiv}>
+              <img src={handMoney} style={{ height: "60%" }} />
               Pay Disbursements
-            </div>
-            <div className={styles.adminItemDiv}>
-              <img src={review} style={{ height: "60%" }}></img>
+            </Link>
+            <Link to="/menu/calendar" className={styles.adminItemDiv}>
+              <img src={review} style={{ height: "60%" }} />
               Review Disbursements
-            </div>
-            <div className={styles.adminItemDiv}>
-              <img src={review2} style={{ height: "60%" }}></img>
+            </Link>
+            <Link to="/menu/calendar" className={styles.adminItemDiv}>
+              <img src={review2} style={{ height: "60%" }} />
               Review Payments
-            </div>
+            </Link>
           </div>
         </div>
       </div>
