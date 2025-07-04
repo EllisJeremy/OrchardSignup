1c0811027cba1f0c786940a02f449d8e3d16d1d0
Jeremy Ellis <jeremyellis173@gmail.com>
2025-07-04 17:46:46 -0400

    feat: all ui done but calendar

--- src/pages/MenuPage/Menu/Menu.tsx
+++ src/pages/MenuPage/Menu/Menu.tsx
@@ -12,17 +12,18 @@ import refund from "../../../assets/refund.svg";
 import handMoney from "../../../assets/handMoney.svg";
 import check from "../../../assets/check.svg";
 import review from "../../../assets/review.svg";
-import review2 from "../../../assets/review2.svg";
+import unlock from "../../../assets/unlock.svg";
+import paperStack from "../../../assets/paperStack.svg";
 
 import scroll from "../../../assets/scroll.svg";
-import calendar from "../../../assets/calendar.svg";
+import mag from "../../../assets/mag.svg";
 
 import recentMessage from "../../../assets/recentMessage.jpg";
 
 import { Link } from "react-router-dom";
 
 const admin: boolean = false;
-const guest: boolean = true;
+const guest: boolean = false;
 let mostRecentMessage: string = "A Summer Series on James";
 if (mostRecentMessage.length >= 30) {
   mostRecentMessage = mostRecentMessage.slice(0, 27) + "...";
@@ -49,34 +50,32 @@ export default function Menu() {
 
         <div className={styles.accountDiv}>
           <div className={styles.headerDiv}>
-            <h2 style={{ textAlign: "right" }}>Account</h2>
+            <h2 style={{ textAlign: "right", marginBottom: "20px" }}>Account</h2>
           </div>
-          <div
-            className={styles.ContainerDiv}
-            style={{ height: "calc(100% - 50px)", flexDirection: "row" }}
-          >
+
+          <div className={styles.ContainerDiv} style={{ height: "calc(100% - 50px)" }}>
             {
               // if a user is a guest give these options
               guest ? (
                 <>
                   <Link to="/" className={styles.accountItemDiv}>
-                    <img src={logIn} style={{ height: "30%" }} />
+                    <img src={logIn} style={{ height: "50%" }} />
                     <h2 style={{ fontSize: "20px" }}>Log In</h2>
                   </Link>
                   <Link to="/signUp" className={styles.accountItemDiv}>
-                    <img src={signUp} style={{ height: "30%" }} />
+                    <img src={signUp} style={{ height: "50%" }} />
                     <h2 style={{ fontSize: "20px" }}>Sign Up</h2>
                   </Link>
                 </>
               ) : (
                 <>
-                  <Link
-                    to="/"
-                    className={styles.accountItemDiv}
-                    style={{ fontSize: "20px", width: "100%" }}
-                  >
-                    <img src={logIn} style={{ height: "30%" }} />
+                  <Link to="/" className={styles.accountItemDiv}>
+                    <img src={logIn} style={{ height: "50%" }} />
                     <h2 style={{ fontSize: "20px" }}>Log Out</h2>
+                  </Link>
+                  <Link to="/" className={styles.accountItemDiv}>
+                    <img src={unlock} style={{ height: "60%" }} />
+                    <h2 style={{ fontSize: "20px" }}>Request Admin</h2>
                   </Link>
                 </>
               )
@@ -158,7 +157,7 @@ export default function Menu() {
           </div>
           <div className={styles.ContainerDiv}>
             <Link to="/menu/calendar" className={styles.adminItemDiv}>
-              <img src={refund} style={{ height: "60%" }} />
+              <img src={mag} style={{ height: "60%" }} />
               View Contributions
             </Link>
             <Link to="/menu/calendar" className={styles.adminItemDiv}>
@@ -174,7 +173,7 @@ export default function Menu() {
               Review Disbursements
             </Link>
             <Link to="/menu/calendar" className={styles.adminItemDiv}>
-              <img src={review2} style={{ height: "60%" }} />
+              <img src={paperStack} style={{ height: "60%" }} />
               Review Payments
             </Link>
           </div>
