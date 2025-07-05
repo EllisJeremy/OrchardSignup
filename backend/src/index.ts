fdea5b1822bd52a2680291de012a39360527f6d6
Jeremy Ellis <jeremyellis173@gmail.com>
2025-07-05 16:55:17 -0400

    feat: new backend setup basic

--- backend/src/index.ts
+++
@@ -1,3 +0,0 @@
-console.log("hello world");
-import { generateRandomNumber } from "./utils";
-generateRandomNumber;
--- backend/src/utils.ts
+++
@@ -1,3 +0,0 @@
-export function generateRandomNumber() {
-  return Math.floor(Math.random() * 100) + 1;
-}
