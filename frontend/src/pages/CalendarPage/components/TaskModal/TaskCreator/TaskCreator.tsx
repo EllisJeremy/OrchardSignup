08802c45100b3e913040577d95c01534c448401a
Jeremy Ellis <jeremyellis173@gmail.com>
2025-07-09 18:05:11 -0400

    feat: slideover done

--- frontend/src/pages/CalendarPage/components/TaskModal/TaskCreator/TaskCreator.tsx
+++ frontend/src/pages/CalendarPage/components/TaskModal/TaskCreator/TaskCreator.tsx
@@ -36,35 +36,33 @@ export default function TaskCreator() {
         <input
           className={styles.titleInput}
           onChange={(e) => setTitle(e.target.value)}
-          placeholder="title"
+          placeholder="Add title"
         ></input>
-
         <div className={styles.typeContainerDiv}>
           <div
-            className={styles.typeDiv}
-            onClick={() => setType("Event")}
-            style={{ background: type === "Event" ? "rgba(0, 180, 255, .5)" : undefined }}
-          >
+            className={styles.typeSlideOverDiv}
+            style={{ left: type === "Task" ? "calc(50% + 5px)" : "0px" }}
+          ></div>
+          <div className={styles.typeDiv} onClick={() => setType("Event")}>
             Event
           </div>
-          <div
-            className={styles.typeDiv}
-            onClick={() => setType("Task")}
-            style={{ background: type === "Task" ? "rgba(0, 180, 255, .5)" : undefined }}
-          >
+          <div className={styles.typeDiv} onClick={() => setType("Task")}>
             Task
           </div>
         </div>
 
-        <TimeInput
-          hour={startHour}
-          minute={startMinute}
-          meridiem={startMeridiem}
-          setHour={setStartHour}
-          setMinute={setStartMinute}
-          setMeridiem={setStartMeridiem}
-        />
-
+        {type === "Event" ? (
+          <TimeInput
+            hour={startHour}
+            minute={startMinute}
+            meridiem={startMeridiem}
+            setHour={setStartHour}
+            setMinute={setStartMinute}
+            setMeridiem={setStartMeridiem}
+          />
+        ) : (
+          <div></div>
+        )}
         <TimeInput
           hour={endHour}
           minute={endMinute}
@@ -73,12 +71,35 @@ export default function TaskCreator() {
           setMinute={setEndMinute}
           setMeridiem={setEndMeridiem}
         />
-
         <textarea
           className={styles.descriptionInput}
           onChange={(e) => setDescription(e.target.value)}
           placeholder="Add description"
         ></textarea>
+
+        <select
+          id="options"
+          className={styles.colorSelect}
+          onChange={(e) => setColor(e.target.value)}
+        >
+          <option value="Red">Red</option>
+          <option value="Orange">Orange</option>
+          <option value="Yellow">Yellow</option>
+          <option value="Green">Green</option>
+          <option value="Blue">Blue</option>
+          <option value="Purple">Purple</option>
+        </select>
+
+        <select
+          id="options"
+          className={styles.colorSelect}
+          onChange={(e) => setColor(e.target.value)}
+        >
+          <option value="">Anyone</option>
+          <option value="example 1">example 1</option>
+          <option value="example 2">example 2</option>
+        </select>
+
         <button
           className={styles.createButton}
           onClick={() => {
@@ -92,20 +113,9 @@ export default function TaskCreator() {
             setTaskDatabase(date, title, startTime, endTime, description, color, "", type);
           }}
         >
+          Create
           <img className={styles.plus} src={plus} />
         </button>
-        <select
-          id="options"
-          className={styles.colorSelect}
-          onChange={(e) => setColor(e.target.value)}
-        >
-          <option value="Red">Red</option>
-          <option value="Orange">Orange</option>
-          <option value="Yellow">Yellow</option>
-          <option value="Green">Green</option>
-          <option value="Blue">Blue</option>
-          <option value="Purple">Purple</option>
-        </select>
       </div>
     );
   }
