29da5759d421dcd079641297e90f44132d40fb8b
Jeremy Ellis <jeremyellis173@gmail.com>
2025-08-18 19:47:00 -0400

    feat: ui change for tasks

--- frontend/src/pages/CalendarPage/components/TaskModal/Task/Task.tsx
+++ frontend/src/pages/CalendarPage/components/TaskModal/Task/Task.tsx
@@ -17,7 +17,7 @@ export default function Task() {
     return (
       <>
         {tasks.map((task, index) => (
-          <div key={index} className={styles.taskDiv}>
+          <div key={index} className={styles.taskCardDiv}>
             <div className={styles.adminBarDiv}>
               <button
                 className={styles.editButton}
@@ -40,47 +40,58 @@ export default function Task() {
                 <img className={styles.trash} src={trash} />
               </button>
             </div>
-            <div
-              className={styles.taskGridDiv}
-              style={task.type === "event" ? { gridTemplateRows: "25px 25px" } : {}}
-            >
-              <p className={styles.title} style={colorMap[task.color]}>
-                {task.title}
-              </p>
+            {task.type === "event" ? (
+              <div>
+                <div className={styles.eventDiv}>
+                  <div style={colorMap[task.color]} className={styles.eventTitleDiv}>
+                    <p className={styles.title}>{task.title}</p>
+                    <p className={styles.time}>
+                      {`${to12Hour(task.startTime)} - ${to12Hour(task.endTime!)}`}
+                    </p>
+                  </div>
 
-              <div className={styles.time}>
-                {to12Hour(task.startTime)}
-                {task.endTime ? ` - ${to12Hour(task.endTime)}` : ""}
+                  <p className={styles.description}>{task.description}</p>
+                </div>
               </div>
+            ) : (
+              <div>
+                <div className={styles.taskDiv}>
+                  <div style={colorMap[task.color]} className={styles.eventTitleDiv}>
+                    <p className={styles.title}>{task.title}</p>
+                    <p className={styles.time}>{to12Hour(task.startTime)}</p>
+                  </div>
+                  <div className={styles.taskContentDiv}>
+                    <p className={styles.description}>{task.description}</p>
+                    {taskDatabase.get(date)![index].owner === null && (
+                      <button
+                        className={styles.selectButton}
+                        onClick={() => {
+                          // TODO add user to task
+                          setRender();
+                        }}
+                      >
+                        <img className={styles.plus} src={check} />
+                      </button>
+                    )}
 
-              <p className={styles.description}>{task.description}</p>
-              {task.type === "s" && taskDatabase.get(date)![index].owner === null && (
-                <button
-                  className={styles.selectButton}
-                  onClick={() => {
-                    // TODO add user to task
-                    setRender();
-                  }}
-                >
-                  <img className={styles.plus} src={check} />
-                </button>
-              )}
-
-              {task.type === "task" && taskDatabase.get(date)![index].owner !== null && (
-                <>
-                  <p className={styles.owner}>{taskDatabase.get(date)![index].owner}</p>
-                  <button
-                    className={styles.unSelectButton}
-                    onClick={() => {
-                      // TODO: remove user from task
-                      setRender();
-                    }}
-                  >
-                    <img className={styles.XFat} src={XFat} />
-                  </button>
-                </>
-              )}
-            </div>
+                    {task.type === "task" && taskDatabase.get(date)![index].owner !== null && (
+                      <>
+                        <p className={styles.owner}>{taskDatabase.get(date)![index].owner}</p>
+                        <button
+                          className={styles.unSelectButton}
+                          onClick={() => {
+                            // TODO: remove user from task
+                            setRender();
+                          }}
+                        >
+                          <img className={styles.XFat} src={XFat} />
+                        </button>
+                      </>
+                    )}
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
         ))}
       </>
