3bd0b36bd4594f5e6540bc7c663e83434452de6e
Jeremy Ellis <jeremyellis173@gmail.com>
2025-08-29 18:47:15 -0400

    feat: header 1/2 done

--- frontend/src/layout/Header/Header.tsx
+++ frontend/src/layout/Header/Header.tsx
@@ -1,11 +1,57 @@
+import { useState } from "react";
 import styles from "./Header.module.css";
 import orchardLogo from "../../assets/OrchardLogoHeader.png";
+import twitter from "../../assets/twitter.svg";
+import instagram from "../../assets/instagram.svg";
+import podcast from "../../assets/podcast.svg";
+
 export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <div className={styles.headerDiv}>
+    <header className={styles.headerDiv} style={menuOpen ? { paddingRight: "300px" } : {}}>
       <div className={styles.headerDivFlex}>
-        <img className={styles.headerLogo} src={orchardLogo} />
+        {/* Logo */}
+        <img className={styles.headerLogo} src={orchardLogo} alt="The Orchard" />
+
+        {/* Desktop Nav */}
+        <nav className={styles.navLinks}>
+          <a href="/index.php">HOME</a>
+          <a href="/about.php">ABOUT US</a>
+          <a href="/visit.php">VISIT US</a>
+          <a href="/media.php">MEDIA</a>
+          <a href="/otherchurches.php">OTHER CHURCHES</a>
+          <a href="/contact.php">CONTACT US</a>
+        </nav>
+        {!menuOpen && (
+          <div className={styles.socials}>
+            <a href="https://twitter.com" target="_blank" rel="noreferrer">
+              <img src={twitter} alt="Twitter" />
+            </a>
+            <a href="https://instagram.com" target="_blank" rel="noreferrer">
+              <img src={instagram} alt="Instagram" />
+            </a>
+            <a href="https://podcasts.com" target="_blank" rel="noreferrer">
+              <img src={podcast} alt="Podcast" />
+            </a>
+          </div>
+        )}
+
+        {/* Burger (mobile only) */}
+        <button className={styles.burgerButton} onClick={() => setMenuOpen(!menuOpen)}>
+          {menuOpen ? "✕" : "☰"}
+        </button>
       </div>
-    </div>
+
+      {/* Side Menu */}
+      <div className={`${styles.sideMenu} ${menuOpen ? styles.showMenu : ""}`}>
+        <a href="/index.php">HOME</a>
+        <a href="/about.php">ABOUT US</a>
+        <a href="/visit.php">VISIT US</a>
+        <a href="/media.php">MEDIA</a>
+        <a href="/otherchurches.php">OTHER CHURCHES</a>
+        <a href="/contact.php">CONTACT US</a>
+      </div>
+    </header>
   );
 }
